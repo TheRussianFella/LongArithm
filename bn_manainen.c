7615ed7bd88a5c9dff12c259ec597eda7f2de407
TheRussianFella <maximmanainen@gmail.com>
2018-11-10 15:09:45 +0300

    Sum+Mul

--- bn_manainen.c
+++ bn_manainen.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 #include "bn.h"
 
@@ -14,8 +15,8 @@ typedef unsigned short bigger_type;
 struct bn_s {
   limb_type* limbs;
 
-  int num_limbs;
-  int occupied_limbs;
+  unsigned int num_limbs;
+  unsigned int occupied_limbs;
 
   char sign;
   int radix;
@@ -47,27 +48,61 @@ int bn_delete(bn* t) {
 
 // Utilities
 
-int bn_increase_size(bn* t) {
+int bn_add_limbs(bn* t, int num_limbs) {
 
   limb_type* old_limbs = t->limbs;
 
-  t->limbs = (limb_type*) calloc(sizeof(limb_type), t->num_limbs*1.25);
+  t->limbs = (limb_type*) calloc(sizeof(limb_type), t->num_limbs+num_limbs);
 
   for (int i = 0; i < t->num_limbs; ++i)
     t->limbs[i] = old_limbs[i];
 
-  t->num_limbs *= 1.25;
+  t->num_limbs += num_limbs;
 
   free(old_limbs);
 
   return BN_OK;
 }
 
+int bn_increase_size(bn* t) {
+  return bn_add_limbs(t, t->num_limbs*0.25);
+}
+
+int bn_digit_shift(bn* t, int num_digit) {
+
+  if (num_digit) {
+    if (t->occupied_limbs+num_digit > t->num_limbs)
+      bn_add_limbs(t, (t->occupied_limbs+num_digit - t->num_limbs)*1.25);
+
+    for (int i = t->occupied_limbs-1; i >= 0; --i){
+      t->limbs[i+num_digit] = t->limbs[i];
+      t->limbs[i] = 0;
+    }
+  }
+
+  t->occupied_limbs += num_digit;
+
+  return BN_OK;
+}
+
+unsigned long long bn_to_decimal(bn* t) {
+  unsigned long long ans = 0;
+
+  for (int i = 0; i < t->num_limbs; ++i)
+    ans += t->limbs[i] * pow(pow(2, sizeof(limb_type)*8), i);
+
+  return ans;
+}
+
+// By limb operations
 
 int bn_add_limb_to_limb(bn* t, limb_type number, int limb_idx) {
 
   limb_type MAX = pow(2, sizeof(limb_type)*8)-1;
   char curry = (MAX - number) < t->limbs[limb_idx];
+
+  if (limb_idx+1 > t->occupied_limbs && number)
+    t->occupied_limbs += 1;
 
   t->limbs[limb_idx] += number;
 
@@ -92,26 +127,21 @@ int bn_multiply_limb_by_limb(bn* t, limb_type number, int limb_idx) {
 
   limb_type curry = (result & ~mask) >> sizeof(limb_type)*8;
 
-  if (limb_idx + 1 == t->num_limbs)
-    bn_increase_size(t);
-  else {
-    if (t->limbs[limb_idx+1] != 0)
-      bn_multiply_limb_by_limb(t, number, limb_idx+1);
-  }
+  if (limb_idx < t->occupied_limbs){
 
-  bn_add_limb_to_limb(t, curry, limb_idx+1);
+    if (limb_idx + 1 == t->num_limbs)
+      bn_increase_size(t);
+    else {
+      //if (t->limbs[limb_idx+1] != 0)
+      bn_multiply_limb_by_limb(t, number, limb_idx+1);
+    }
+
+    bn_add_limb_to_limb(t, curry, limb_idx+1);
+  }
 
   return BN_OK;
 }
 
-int bn_to_decimal(bn* t) {
-  int ans = 0;
-
-  for (int i = 0; i < t->num_limbs; ++i)
-    ans += t->limbs[i] * pow(pow(2, sizeof(limb_type)*8), i);
-
-  return ans;
-}
 // Initialization
 
 int bn_init_string_radix(bn *t, const char *init_string, int radix) {
@@ -132,19 +162,103 @@ int bn_init_string_radix(bn *t, const char *init_string, int radix) {
   return BN_OK;
 }
 
+int bn_init_string(bn* t, const char *init_string) {
+  return bn_init_string_radix(t, init_string, 10);
+}
+
+bn *bn_init(bn const *orig) {
+
+  bn* new = (bn*) malloc(sizeof(bn));
+
+  new->limbs = (limb_type*) calloc(sizeof(limb_type), orig->num_limbs);
+  new->num_limbs = orig->num_limbs;
+
+  new -> sign = orig -> sign;
+  new -> radix = orig -> radix;
+
+  for (int i = 0; i < orig -> occupied_limbs; ++i)
+    new->limbs[i] = orig->limbs[i];
+
+  new -> occupied_limbs = orig -> occupied_limbs;
+
+  return new;
+}
+
+int bn_equals_init(bn* t, bn const *orig) {
+
+  if (t->num_limbs > orig->occupied_limbs) {
+
+    memset(t->limbs, 0, t->num_limbs);
+    for (int i = 0; i < orig->occupied_limbs; ++i)
+        t->limbs[i] = orig->limbs[i];
+
+    t -> occupied_limbs = orig->occupied_limbs;
+    t -> radix = orig -> radix;
+    t -> sign = orig -> sign;
+
+  } else {
+
+    bn_delete(t);
+    t = bn_init(orig);
+  }
+
+  return BN_OK;
+}
+
+// One argument operations
+
+int bn_add_to(bn *t, bn const *right) {
+
+  for (int i = 0; i < right->occupied_limbs; ++i)
+    bn_add_limb_to_limb(t, right->limbs[i], i);
+
+  return BN_OK;
+}
+
+// Two argument operations
+
+bn* bn_add(bn const *left, bn const *right) {
+
+  bn* answer = bn_init(left);
+  bn_add_to(answer, right);
+
+  return answer;
+}
+
+bn* bn_mul(bn const *left, bn const *right) {
+
+  bn* answer = bn_new();
+  bn* temp = bn_init(left);
+
+  for (int i = 0; i < right->occupied_limbs; ++i) {
+
+    bn_multiply_limb_by_limb(temp, right->limbs[i], 0);
+    bn_digit_shift(temp, i);
+
+    bn_add_to(answer, temp);
+
+    bn_equals_init(temp, left);
+  }
+
+  return answer;
+}
+
 ///////////////////////
 
 int main() {
 
-  bn* big_number = bn_new();
+  bn* first = bn_new();
+  bn* second = bn_new();
 
-  bn_init_string_radix(big_number, "101010101010010101", 2);
+  bn_init_string_radix(first, "12", 10);
+  bn_init_string_radix(second, "1201", 10);
 
-  printf("%d %d %d\n", big_number->limbs[0], big_number->limbs[1], big_number->limbs[2]);
-  printf("%d", bn_to_decimal(big_number));
+  bn* ans = bn_mul(first, second);
 
+  printf("\n%llu * %llu = %llu\n", bn_to_decimal(first), bn_to_decimal(second), bn_to_decimal(ans));
 
-  bn_delete(big_number);
+  bn_delete(first);
+  bn_delete(second);
 
   return 0;
 }
